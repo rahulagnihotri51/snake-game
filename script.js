54b85dd6420d4dc49c8a1bbb263f9cf67f93e2fe
Rahul Agnihotri <rahulthe64529@gmail.com>
2026-04-06 14:27:21 +0530

    feat: initialize snake body and blocks, update board rendering logic

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const blocks=[];
+const snake=[{
+    x:1,y:2
+},{
+    x:1,y:3
+},{
+    x:1,y:4
+}];
 document.addEventListener('DOMContentLoaded', () => { //this wait until the entire html page is loaded
     const board = document.querySelector('.board');
     const blockHeight = 50;
@@ -7,8 +15,7 @@ document.addEventListener('DOMContentLoaded', () => { //this wait until the enti
         console.error('Board element not found');
         return;
     }
-    board.innerHTML = ''; //this line has renmoved the white lines at the bottom of board. it cleans allt he things
-
+    board.innerHTML = ''; //this line has removed the white lines at the bottom of board. it cleans allt he things
     const rect = board.getBoundingClientRect(); //gets the actual pixel size of the board
     const rows = Math.floor(rect.height / blockHeight);
     const cols = Math.floor(rect.width / blockWidth);
@@ -18,6 +25,12 @@ document.addEventListener('DOMContentLoaded', () => { //this wait until the enti
             const block = document.createElement('div');
             block.classList.add('blocks');
             board.appendChild(block);
+            blocks[`${row}-${col}`]=block;
         }
     }
 });
+function render(){
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+    });
+}
